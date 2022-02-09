5c79bfab79ee5ad4898d8fc43388145b9d571432
Nathan Papes <nathan.papes@gmail.com>
2022-02-09 13:39:44 -0500

    Improve docker env for development and production builds

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import {Nav} from "./components/Nav";
 import {Home} from "./views/Home";
 import {About} from "./views/About";
 import {NotFound} from "./views/NotFound";
+
+console.log("vite env metadata", import.meta.env);
 
 export const App: React.FC = () => {
   return (
@@ -12,7 +14,8 @@ export const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route element={<NotFound />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
