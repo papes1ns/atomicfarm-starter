bd7e5e349e0009ee6072d8149dd2f12b5fe707ce
Nathan Papes <nathan.papes@gmail.com>
2022-02-11 14:10:40 -0500

    Add version info to fronend production builds

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "RAILS_ENV == $RAILS_ENV"
-
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -e
 
-echo "NODE_ENV == $NODE_ENV"
+inject_javascript=$(cat <<EOF
+window.globals = { \
+  VERSION: "$VERSION", \
+};
+EOF
+)
+
+echo "Injecting javascript into index.html"
+echo $inject_javascript
+
+sed -i "s|window\.globals\;|$inject_javascript|g" html/index.html
 
 exec "$@"
+
+
